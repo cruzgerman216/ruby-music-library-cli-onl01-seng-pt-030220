c920ba8d66e73b3b619d1645a055f1bac8643716
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 19:16:11 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,10 @@
 class Genre
-  attr_accessor :name, :songs, :artists
+  attr_accessor :name, :songs
   @@all = []
 
   def initialize(name)
     @name = name
     @songs = []
-    @artists = []
     save
   end
 
