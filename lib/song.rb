0280a2354a94baa583b4689a2a401dd5732ab63b
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 19:45:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-
+    if !self.find_by_name(name)
+       Song.new(name)
+     else
+       self.find_by_name(name)
+    end
   end
 
 end
