627839c476bb62bbbc4876eb551cdf3e9e4961ba
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 19:38:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,11 +40,9 @@ class Song
   end
 
   def self.find_by_name(name)
-    song_instance = nil
-    Song.all.each do |song|
-      song_instance = song if song.name == name
-    end
-    song_instance
+    @@all.detect do |song|
+        song.name == name
+      end
   end
 
   def self.find_or_create_by_name(name)
