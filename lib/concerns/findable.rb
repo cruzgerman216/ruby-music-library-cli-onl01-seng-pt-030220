eec4d38a84d3b47e0f6f60d34306495710c03b61
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 20:32:14 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -11,8 +11,12 @@ module Concerns::Findable
     instance
   end
 
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+       self.create(name)
+     else
+       self.find_by_name(name)
+    end
   end
 
 end
