ac87949ed3922357619ee6c7e0148e13af388f7c
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 21:41:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,8 +60,4 @@ class Song
     song.artist = Artist.find_or_create_by_name(arr[0])
     song
   end
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    #artist.add_song(self)
-  end
 end
