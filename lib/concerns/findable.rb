df3e2dbf3851ff54d4d281cfb4499b9f2e952b01
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 20:32:03 +0000

    Automatically backed up by Learn

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,18 @@
+module Concerns::Findable
+  def find_by_name(name)
+    self.all.detect {|e| e.name == name}
+  end
+
+  def self.find_by_name(name)
+    instance = nil
+    self.all.each do |element|
+      instance = song if name.name == name
+    end
+    instance
+  end
+  
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create(name)
+  end
+
+end
