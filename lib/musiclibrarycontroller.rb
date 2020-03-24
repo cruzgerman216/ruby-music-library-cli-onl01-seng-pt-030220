6c4b2dca18b86c7092d588e802b8ec69c582d292
German Cruz <cruzgerman216@yahoo.com>
2020-03-24 22:22:12 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -3,4 +3,8 @@ class MusicLibraryController
   def initialize(path = './db/mp3s')
     mi = MusicImporter.new(path).import
   end
+
+  def call
+    puts "welcome to your music library!"
+  end
 end
