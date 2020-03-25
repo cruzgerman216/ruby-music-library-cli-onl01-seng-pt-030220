ac48b724c58885f7da1c1e0062b68681b50893b6
German Cruz <cruzgerman216@yahoo.com>
2020-03-25 00:55:00 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -49,6 +49,7 @@ class MusicLibraryController
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
+    input = gets.strip
   #  art.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
   #      puts "#{i+1}. #{s.name} - #{s.genre.name}"
   #    end
