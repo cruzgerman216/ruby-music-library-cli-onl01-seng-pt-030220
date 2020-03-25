07b46836f88f91c7919883a3049e085a9d9aa922
German Cruz <cruzgerman216@yahoo.com>
2020-03-25 00:17:51 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -37,7 +37,7 @@ class MusicLibraryController
 
   def list_artists
     Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
-      puts "#{i+1}. #{a.name}"
+      puts "4"
     end
   end
 end
